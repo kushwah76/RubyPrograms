c7515ccdfa192845b2ba3f651e0f8d77d1961711
kushwah76 <ankushwah@bestpeers.com>
2022-12-22 16:57:48 +0530

    Date And Time Example

---
+++ DateAndTimes.rb
@@ -0,0 +1,9 @@
+time=Time.new
+
+puts time.inspect
+puts  time.year
+puts time.month # => Month of the date (1 to 12)
+puts time.day # => Day of the date (1 to 31 )
+puts time.wday # => 0: Day of week: 0 is Sunday
+puts time.yday  # => 365: Day of year
+puts  time.hour  # => 23: 24-hour clock
