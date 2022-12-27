a886d8ccbfe8614f12e49c36a39cc3f23ceb0acd
kushwah76 <ankushwah@bestpeers.com>
2022-12-27 12:42:35 +0530

    arrayMethods

---
+++ arrayMethods.rb
@@ -0,0 +1,31 @@
+array=[10,2,10,1,45,2111]
+#puts array.length
+
+#puts array.first
+#puts array.last
+#puts array.take(2)
+#puts array.drop(2)
+#puts array[0]
+#puts array.pop
+#puts array.shift
+#puts array.push("ankit")
+#puts array.unshift(1111)
+
+#puts array.reverse
+#puts array.select{|number| number>10}
+#puts array.include?(2)
+#puts array.join("*")
+
+#puts array.map{|element| element*2}
+#puts array.uniq
+#puts array.concat([1,2,3,4,5],[5,4,3,2,1])
+#puts array.sort
+#puts array.drop_while{|i| i<30}
+#puts array.inject(:*)
+#puts array.min
+#puts array.max
+#puts array.minmax
+#puts array.min_by(2){|i| i}
+#puts array.max_by(2){|i| i}
+#puts array.sum(2)
+#puts array.take_while{|i| i<3}
