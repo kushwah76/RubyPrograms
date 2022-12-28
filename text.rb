c6d8e06cb26926223115ef106af29b59ad0e3743
kushwah76 <ankushwah@bestpeers.com>
2022-12-28 12:45:05 +0530

    text.rb

--- text.rb
+++ text.rb
@@ -1,3 +1,49 @@
-a=10
-b=20
-puts c=a+b
+=begin
+$hash={}
+
+def addData
+  
+  
+  puts "enter key name:-"
+  keys=gets.chomp
+  puts "enter value:-"
+  values=gets.chomp
+ 
+  
+  
+  $hash[keys.to_sym]=values
+  a
+
+end 
+
+  
+  def a 
+puts "Enter 1 for Each Iterator"
+puts "Enter 2 show"
+puts "exit"
+print  "Choose the operation you want to perform:- "   
+choose=gets.chomp
+case choose
+when "1"
+  addData
+when "2"
+  puts $hash
+  a
+
+when "3"
+  exit
+
+else
+  puts "wrong choicproc=Proc.new {|x,y| puts x,y}
+ proc.call(1,"jj")
+ return 1
+ proc.call(1,2)es"
+end
+end
+a
+=end
+
+arr=[10,20,1,34,40,60]
+arr.each_with_index do |value,index|
+  puts "#{value} : #{index}"
+end
